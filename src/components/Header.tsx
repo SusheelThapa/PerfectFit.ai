ee59b3f09b439d95e90bf63cccf83f8977410af9
SusheelThapa <susheelthapa90@gmail.com>
2024-07-14 15:15:54 +0545

    feat: created landing page

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="flex justify-between py-4 items-center">
+    <div className="flex justify-between my-2 items-center h-[10vh] border-b-2">
       <div className="text-4xl font-black tracking-widest">PerfectFit.ai</div>
       <div>
         <ul className="flex gap-8 text-lg">
